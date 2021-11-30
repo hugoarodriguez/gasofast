a57c5e2ffe6c7bc69304291eeb591770a7d27e5f
Hugo Rodríguez <hugsrodriguez8@gmail.com>
2021-11-30 17:29:50 -0600

    Agregar - quitar gasolineras favoritas

--- lib/src/providers/gasolinera_provider.dart
+++ lib/src/providers/gasolinera_provider.dart
@@ -5,6 +5,85 @@ class GasolineraProvider{
 
   final cloud_firestore.FirebaseFirestore _firestore = cloud_firestore.FirebaseFirestore.instance;
 
+  //Agregar Gasolinera Favorita
+  Future<bool> aggGasolineraFavorita(String uid, GasolineraModel gasolineraModel) async {
+    
+    cloud_firestore.CollectionReference reference  = _firestore.collection('favorite_gas_stations');
+
+    try{
+      
+      //Registrar
+      await reference.doc(gasolineraModel.id + uid).set({
+        'id_user'             : uid,
+        'id_gas_station'      : gasolineraModel.id,
+        'location_latitude'   : gasolineraModel.locationLatitude,
+        'location_longitude'  : gasolineraModel.locationLongitude,
+        'name'                : gasolineraModel.name,
+        'price_diesel'        : gasolineraModel.priceDiesel,
+        'price_especial'      : gasolineraModel.priceEspecial,
+        'price_regular'       : gasolineraModel.priceRegular,
+        'cover_img'           : gasolineraModel.coverImg,
+      });
+      //Si la subida salió bien se retorna 'true'
+      return true;
+      
+    } on cloud_firestore.FirebaseException catch(e){
+      //Si hubo error en la subida se retorna 'false'
+      print(e);
+      return false;
+    }
+  }
+
+  //Eliminar Gasolinera Favorita
+  Future<bool> elmGasolineraFavorita(String uid, GasolineraModel gasolineraModel) async {
+    
+    cloud_firestore.CollectionReference reference  = _firestore.collection('favorite_gas_stations');
+
+    try{
+      
+      //Registrar
+      await reference.doc(gasolineraModel.id + uid).delete();
+
+      //Si la subida salió bien se retorna 'true'
+      return true;
+      
+    } on cloud_firestore.FirebaseException catch(e){
+      //Si hubo error en la subida se retorna 'false'
+      print(e);
+      return false;
+    }
+  }
+
+  //Evaluamos si la gasolinera es favorita
+  Future<bool> isFavorita(String uid, String idGasolinera) async {
+
+    String idGasolineraFinal = idGasolinera;
+
+    if(idGasolinera.contains('(')){
+      int indexParentesis1 = idGasolinera.indexOf('(');
+      int indexParentesis2 = idGasolinera.indexOf(')');
+      idGasolineraFinal = idGasolinera.substring(indexParentesis1 + 1, indexParentesis2);
+    }
+
+    cloud_firestore.CollectionReference reference  = _firestore.collection('favorite_gas_stations');
+    bool r = false;
+
+    try{
+      
+      //Registrar
+      final cloud_firestore.DocumentSnapshot documentSnapshot = await reference.doc(idGasolineraFinal + uid).get();
+
+      r = documentSnapshot.exists;//Retorna 'true' si existe, 'false' si no
+      
+      return r;
+      
+    } on cloud_firestore.FirebaseException catch(e){
+      //Si hubo error en la consulta retorna 'false', indica que no existe
+      print(e);
+      return false;
+    }
+  }
+
   Future<List<GasolineraModel>> getGasolineras() async {
     List<GasolineraModel> gasolinerasList = [];
 
@@ -30,8 +109,6 @@ class GasolineraProvider{
       int indexParentesis2 = idGasolinera.indexOf(')');
       idGasolineraFinal = idGasolinera.substring(indexParentesis1 + 1, indexParentesis2);
     }
-
-    
 
     List<GasolineraModel> gasolinerasList = [];
     
--- lib/src/widgets/fuel_station_widget.dart
+++ lib/src/widgets/fuel_station_widget.dart
@@ -1,29 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:gasofast/src/models/gasolinera_model.dart';
 
 import 'package:gasofast/src/providers/gasolinera_provider.dart';
 import 'package:gasofast/src/utils/colors_utils.dart';
 import 'package:gasofast/src/utils/styles_utils.dart';
 
 // ignore: must_be_immutable
-class FuelStation extends StatelessWidget {
-
-  final gasolineraProvider = GasolineraProvider();
+class FuelStation extends StatefulWidget {
 
   String gasStationId;
 
   FuelStation({required this.gasStationId});
 
   @override
+  State<FuelStation> createState() => _FuelStationState();
+}
+
+class _FuelStationState extends State<FuelStation> {
+  final firebase_auth.FirebaseAuth auth = firebase_auth.FirebaseAuth.instance;
+
+  final firebase_auth.User? user = firebase_auth.FirebaseAuth.instance.currentUser;
+
+  final gasolineraProvider = GasolineraProvider();
+
+  List<GasolineraModel> datosGasolinera = [];
+
+  @override
+  void initState() {
+    super.initState();
+    isFavorita();
+  }
+
+  
+  Icon favoriteUnchecked = Icon(Icons.star_outline, color: colorAzulOscuro(), size: 20.0,);
+  Icon favoriteChecked = Icon(Icons.star, color: colorAzulOscuro(), size: 20.0,);
+  bool _favoriteIsChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return _gasCardView(context);
   }
 
+  void isFavorita() async{
+    //Evaluamos si la gasolinera está marcada como favorita para este usuario
+    _favoriteIsChecked = await gasolineraProvider.isFavorita(user!.uid, widget.gasStationId);
+  }
+
   Widget _gasCardView(BuildContext context){
     return FutureBuilder(
-      future: gasolineraProvider.getGasolinera(gasStationId),
+      future: gasolineraProvider.getGasolinera(widget.gasStationId),
       builder: (BuildContext context, AsyncSnapshot snapshot){
 
-        if(snapshot.hasData){
+        if(snapshot.hasData) {
+
+          //Obtenemos los datos de la gasolinera que se va a guardar o eliminar de favoritos
+          datosGasolinera.add(snapshot.data[0]);
+
           return Card(
             margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0 ),
             elevation: 10.0,
@@ -40,7 +73,7 @@ class FuelStation extends StatelessWidget {
                     height: 80.0,
                     ),
                   ),
-                _contentRow(context, snapshot.data[0].name, snapshot.data[0].name)
+                _contentRow(context, snapshot.data[0].name, snapshot.data[0].name),
               ],
             ),
           );
@@ -60,7 +93,7 @@ class FuelStation extends StatelessWidget {
                     height: 80.0,
                     ),
                   ),
-                _contentRow(context, 'Cargando...', 'Cargando...')
+                _contentRow(context, 'Cargando...', 'Cargando...'),
               ],
             ),
           );
@@ -70,6 +103,7 @@ class FuelStation extends StatelessWidget {
   }
 
   Widget _contentRow(BuildContext context, String gasStationName, String gasStationSchedule){
+
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10.0),
       child: Column(
@@ -80,8 +114,37 @@ class FuelStation extends StatelessWidget {
             children: <Widget>[
               Text(gasStationName, style: TextStyle(color: colorAzulOscuro(), fontSize: 12.0, fontWeight: FontWeight.bold ),),
               IconButton(
-                icon: Icon(Icons.star_outline, color: colorAzulOscuro(), size: 20.0,),
-                onPressed: (){},
+                icon: _favoriteIsChecked ? favoriteChecked : favoriteUnchecked,
+                onPressed: () async {
+
+                  String uid = user!.uid;
+                  bool r = false;
+
+                  if (_favoriteIsChecked){
+
+                    //Quitar de favoritos
+                    r = await gasolineraProvider.elmGasolineraFavorita(uid, datosGasolinera[0]);
+
+                  } else {
+
+                    //Agregar a favoritos
+                    r = await gasolineraProvider.aggGasolineraFavorita(uid, datosGasolinera[0]);
+                  }
+                  
+                  setState((){
+                    if (_favoriteIsChecked && r){
+
+                      //Quitar de favoritos
+                      _favoriteIsChecked = false;
+
+                    } else if (!_favoriteIsChecked && r){
+
+                      //Agregar a favoritos
+                      _favoriteIsChecked = true;
+                    }
+                  });
+
+                },
               )
             ],
           ),
